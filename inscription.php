3fd61d34d35e2d2b9b6ede11a454f1bdc7390184
franperna <frpernayan@free.fr>
2016-05-17 14:05:26 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<?php session_start();
+?>
 <html>
 
 <head>
@@ -8,28 +10,34 @@
 </head>
 
 
-<form method ="post" >
+<form method="post"  enctype="multipart/form-data">
 <section>
 <article>
        <legend>Inscription</legend>
-	   
+
 <p>
 
-
-
-<label for="pseudo">Pseudo :</label>
-<input type ="text" name="pseudo" id="pseudo" placeholder="Pseudo.." required/>
+<label for="firstname">Nom :</label>
+<input type ="text" name="nom"  value="<?php if (isset($_SESSION['nom'])){echo htmlentities($_SESSION['nom']);} ?>"id="nom" placeholder="Nom.." requered/>
 <br/>
+<br/>
+<label for="lastname">Prenom :</label>
+<input type ="text" name="prenom"  value="<?php if (isset($_SESSION['prenom'])){echo htmlentities($_SESSION['prenom']);} ?>"id="prenom" placeholder="Prenom.."requered/>
+
+
+<br/>
+
+
 <br/>
 <label for="pass">Mot de passe :</label>
-<input type ="password" name="mot_de_passe" id="mot_de_passe" placeholder="Mot de passe.." required/>
+<input type ="password" name="mot_de_passe"  id="mot_de_passe" placeholder="Mot de passe.." required/>
 <br/>
 
 <br/>
 <label for="passcon">Confirmation mot de passe :</label></td>
-<input type ="password" name="passcon" id="passcon" placeholder="Mot de passe.." required/>
+<input type ="password" name="passcon"  id="passcon" placeholder="Mot de passe.." required/>
 
-
+<br/>
 
 <br/>
 Sexe :
@@ -38,61 +46,53 @@ Sexe :
 
 <br/>
 <br/>
-<label for="firstname">Nom :</label>
-<input type ="text" name="nom" id="nom" placeholder="Nom.." requered/>
-<br/>
-<br/>
-<label for="lastname">Prenom :</label>
-<input type ="text" name="prenom" id="prenom" placeholder="Prenom.."requered/>
-<br/>
-
-<br/>
 
 
-<label>Date de Naissance  </label><input type="date" name= "birthday" max="2016-01-01" min="1900-01-01">(jj-mm-aaaa)<br/><br/>
 
-  
+ <label>Date de Naissance  </label><input type="date" name= "birthday" value="<?php if (isset($_POST['date'])){echo htmlentities($_POST['date']);} ?>" max="2016-01-01" min="1900-01-01">(jj-mm-aaaa)<br/>
 
 
-<br/>
+
+
 <br/>
 <label for="phone">Numero de telephone :</label>
-<input type ="tel" name="phone" placeholder="06.."/>
+<input type ="tel" name="phone"  value="<?php if (isset($_POST['phone'])){echo htmlentities($_POST['phone']);} ?>"placeholder="06.."/>
 <br/>
 <br/>
 <label for="email1">Email :</label>
-<input type ="email" name="mail" id="email1" placeholder="Email.."requered//>
+<input type ="email" name="mail"  value="<?php if (isset($_SESSION['mail'])){echo htmlentities($_SESSION['mail']);} ?>"id="email1" placeholder="Email.."requered/>
 <br/>
 
 <br/>
 <label for="email2">Confirmation Email :</label>
-<input type ="email" name="email2" id="email2" placeholder="Email.." required/>
+<input type ="email" name="email2" value="<?php if (isset($_SESSION['mail'])){echo htmlentities($_SESSION['mail']);} ?>"id="email2" placeholder="Email.." required/>
 
 <br/>
 <br/>
 <label for="photo">Photo :</label>
-<input type="file" name="logo"/>
+<input type="file" name="avatar"/>
 <input type="hidden" name="MAX_FILE_SIZE" value="100000">
 <br/>
 
 <br/>
 <label for="description"> Description </label><br/>
-<textarea name="description" id="description"></textarea>
+<textarea type="text" name="description" value="<?php if (isset($_POST['description'])){echo htmlentities($_POST['description']);} ?>"id="description"></textarea>
+
 <br/>
 <br/>
 
-<input type="checkbox" name="Regle"> Reglement à valider </a></br></br>
- 
+<input type="checkbox" name="Regle">Je reconnais avoir lu et approuvé les conditions d’utilisation </a></br></br>
+
 <br/>
 
 
-<input type="reset" value="Reset"/>
 
 <input type="submit" name= Envoyer value="Envoyer"/>
 
 </p>
 
 </form>
+</article>
 <?php
 try
 {
@@ -104,30 +104,30 @@ catch (Exception $e)
 }
  if(isset($_POST['Envoyer']))
     {
-    
-      
+
+
       // Je recupere les infos, plus securité pour le code.
         htmlentities($mot_de_passe = $_POST['mot_de_passe']);
         htmlentities($passcon = $_POST['passcon']);
         htmlentities($mail = $_POST['mail']);
         htmlentities($email2 = $_POST['email2']);
-        htmlentities($pseudo= $_POST['pseudo']);
+        //htmlentities($pseudo= $_POST['pseudo']);
         htmlentities($civilite = $_POST['civilite']);
         htmlentities($prenom = $_POST['prenom']);
         htmlentities($nom = $_POST['nom']);
         htmlentities($birthday = $_POST['birthday']);
-        
+
         //htmlentities($user_post_office_box = $_POST['user_post_office_box']);
-        htmlentities($city = $_POST['city']);
+       // htmlentities($city = $_POST['city']);
         htmlentities($Regle=$_POST['Regle']);
 		htmlentities($description=$_POST['description']);
-    
+
         // empecher les codes php dans la base
       // Je verifie que TOUT les champs sont remplis.
       if(empty($mot_de_passe)||empty($mail)|| empty($Regle)|| empty($prenom) || empty($prenom) || empty($birthday )  || empty($mail)|| empty($description) )
       {
-                    $message0="Vous devez remplir toutes les coordonnées";
-                      echo '<script type="text/javascript">window.alert("'.$message0.'"); window.location.href="testinscription.php";</script>';
+                    echo "Vous devez remplir tout les formulaires";
+                      //echo '<script type="text/javascript">window.alert("'.$message0.'"); window.location.href="testinscription.php";</script>';
       }
 	  // filter_var permet de verifier la validité de l'email
       else {
@@ -135,14 +135,25 @@ catch (Exception $e)
               $sql = $bdd->prepare('SELECT mail FROM utilisateur WHERE mail = \'' . $mail . '\';');
               $sql->execute(array('.$mail.' => $_POST['mail']));
               $res = $sql->fetch();
-              if ($res) {
+              /*if ($res) {
                   $message3 = "L\'Email que vous avez utilisé existe déjà";
                   echo '<script type="text/javascript">window.alert("' . $message3 . '"); window.location.href="testinscription.php";</script>';
-              } else {
+              } else {*/
                   if (($mot_de_passe == $passcon) && ($mail == $email2)) {
 					  $pass_hache = sha1($_POST['mot_de_passe']);
-                      $reponse = $bdd->query("INSERT INTO utilisateur VALUES('$nom', '$prenom','$mail', '$mot_de_passe', '$birthday', '$description','$civilite','$pseudo'");
-					 /*$req = $bdd->prepare('INSERT INTO utilisateur (nom,prenom,mail,mot_de_passe,age,civilite,pseudo) VALUES(?,?,?,?,?,?,?)');
+					  $req = $bdd->prepare('SELECT id FROM utilisateur WHERE mail = ? ');
+						$req->execute(array(
+						$_POST['mail']));
+						//print_r ($req['id']);
+						//var_dump $req;
+						//die();
+						$resul= $req->fetch();
+							
+						var_dump($resul['id']);
+						
+						
+					$reponse = $bdd->query('UPDATE  utilisateur SET nom=\''.$nom.'\', prenom=\''.$prenom.'\', mail=\''.$mail.'\',mot_de_passe=\''.$pass_hache.'\',age=\''.$birthday.'\',description=\''.$description.'\', civilite=\''.$civilite.'\' WHERE id=\''.$resul['id'].'\'');
+					  /*$req = $bdd->prepare('INSERT INTO utilisateur (nom,prenom,mail,mot_de_passe,age,civilite,pseudo) VALUES(?,?,?,?,?,?,?)');
 						$req->execute(array(
 						$nom,
 						$prenom,
@@ -151,30 +162,70 @@ catch (Exception $e)
 						$birthday,
 						$civilite,
 						$pseudo
-						));*/
+					));*/
+						
                       //affiche un mot gentil, dans le futur on doit changer pour que ceci apparaisse sur une autre.nom,prenom,mail,mot_de_passe,age,civilite,pseudo
-                      $message = "Bonjour $prenom votre compte est bien enregistré";
-                      echo '<script type="text/javascript">window.alert("' . $message . '"); window.location.href="testinscription.php";</script>';
+					
+						
+	
+					if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name'])){
+					$taillemax=2097152;
+						$extensionsvalides=array('jpg','jpeg','gif','png');
+					if($_FILES['avatar']['size'] <= $taillemax){
+					$extensionupload = strtolower(substr(strrchr($_FILES['avatar']['name'],'.'),1)); // check si tout minuscule et l'extension
+						if(in_array($extensionupload, $extensionsvalides)){
+							$chemin = "IMG/avatar/".$resul['id'].".".$extensionupload;
+							$resultat = move_uploaded_file($_FILES['avatar']['tmp_name'],$chemin); //stockage temporaire
+							if($resultat){
+								$req = $bdd->prepare('UPDATE utilisateur SET photo = :avatar WHERE id = :idutilisateur');
+								$req-> execute(array(
+								'avatar' => $resul['id'].".".$extensionupload,
+								'idutilisateur' => $resul['id']
+								));
+           
+							}
+								else{
+									$msg = "Erreur lors de l'importation de la photo de profil...";
+								}
+							}
+								else{
+									$msg = "Votre photo de profil doit être au format indiqué !";
+								}
+					}
+						else{
+							$msg = "Votre photo de profil ne doit pas dépasser 2 Mo !";
+						}
+					}
+                      echo "Bonjour $prenom votre compte a bien été modifié";
+					   session_start();
+						
+						$_SESSION['nom'] = $_POST['nom'];
+						$_SESSION['prenom'] = $_POST['prenom'];
+						$_SESSION['mot_de_passe'] = $_POST['mot_de_passe'];
+						$_SESSION['mail'] = $_POST['mail'];
+						$_SESSION['age']=$_POST['birthday'];
+						$_SESSION['description']=$_POST['description'];
+						$_SESSION['civilite']=$_POST['civilite'];
+						header('location: profil.php');
+                      //echo '<script type="text/javascript">window.alert("' . $message . '"); window.location.href="testinscription.php";</script>';
                   } else {
-                      $message1 = "Les deux mots de passe ou les deux e-mails que vous avez rentrés ne correspondent pas";
-                      echo '<script type="text/javascript">window.alert("' . $message1 . '"); window.location.href="testinscription.php";</script>';
+                      echo "Les deux mots de passe ou les deux e-mails que vous avez rentrés ne correspondent pas";
+                      //echo '<script type="text/javascript">window.alert("' . $message1 . '"); window.location.href="testinscription.php";</script>';
                   }
-              }
+              /*}*/
           } else {
-              $message2 = "Votre email n\'est pas valide";
-              echo '<script type="text/javascript">window.alert("' . $message2 . '"); //window.location.href="testinscription.php";</script>';
+             echo "Votre email n\'est pas valide";
+              //echo '<script type="text/javascript">window.alert("' . $message2 . '"); //window.location.href="testinscription.php";</script>';
           }
       }
     }
-    
+
 ?>
 </table>
 </div>
 
-</article>
+
 </section>
 
 </body>
 </html>
-
-
