c07290ce166d4009d91708c5bb74d7269a33a969
franperna <frpernayan@free.fr>
2016-05-30 11:56:37 +0200

    Add files via upload

---
+++ cssbackoffice.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="cssbackoffice.css">
+  <title>BackOfficeAccueil</title>
+</head>
+
+<body>
+
+
+<nav class="menu" style="width:18%;">
+  <header class="container1">
+    <br>
+    <a href="logo1.php"><img class="imageconnexion" src="logo3.jpg" alt="" /></a>
+    <div style="margin-left: 25%; text-align: right;">
+      <br><br>
+    <a class="boutton" href="accueil.php">&nbsp;Accueil&nbsp;</a><br><br>
+    <a class="boutton" href="sports.php">&nbsp;&nbsp;Sports&nbsp;&nbsp;</a><br><br>
+    <a class="boutton" href="groupes.php">&nbsp;Groupes</a><br><br>
+    <a class="boutton" href="forum.php">&nbsp;&nbsp;Forum&nbsp;&nbsp;</a><br><br>
+    <a class="boutton" href="reglage.php">&nbsp;&nbsp;FAQ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><br><br>
+	
+  </div>
+</nav>
+
+
+<div style="margin-left:18%">
+<header class="container" style="background-color:rgb(128, 75, 144);color:white;width:95%;position:fixed;">
+
+
+  <h1 style="text-align:center;">Back-Office</h1>
+  <h3 style="text-align:center;">Accueil</h3>
+</header>
+</div>
---
+++ en-tete.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="en-tete.css">
+  <title>accueil</title>
+</head>
+<body background="fond.jpg" style="width:100%">
+<header>
+<div class="container" style="background-color:#404040;color:white;width:100%;position:fixed;z-index:3;">
+<h1 style="text-align:center;">On est la pour faire du sport</h1>
+
+<div id="im">
+    <a href="logo.php"><img class="imageconnexion" src="logo3.jpg" alt="" /></a>
+</div>
+</div>
+<br>
+
+<div id="co1">
+	<a class="boutton" href="Inscription.html"> S'inscrire  </a></li> 
+ <a class="boutton" href="Connexion.html"> Se connecter </a></li>
+</div>
+<div id="menu">
+  <ul id="onglets">
+   <li class="active"><a href="Accueil.html"> Accueil </a></li>
+    	<li><a href="Forums.html"> groupe </a></li>
+		<li><a href="Livre_or.html"> sport </a></li>
+		<li><a href="forum.html"> forum</a></li>
+		<li><a href="recherche.html"> recherche</a></li>
+  </ul>
+</div>
+</header>
+
+
+<section>
+<p> Nouveauté </p>
+
+</section>
+ 
+</body>
