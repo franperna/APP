7ad0670b76323be175ac4231826eec40c88c9e75
franperna <frpernayan@free.fr>
2016-05-30 14:26:27 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -1,100 +1,59 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="general.css" />
-        <title> le site du sport </title>
-    </head>
+<head>
+		<link rel="stylesheet" href="general.css"/>
+		<meta charset ="utf-8"/>
+		<title>profil</title>
+</head>
 
-    <body>
-        <div id="bloc_page">
-             <header>
-                 
-                     <a href="Images site web/12650659_886332674816078_425547975_n.jpeg">
+<?php session_start();
+?>
 
+<?php
 
-                     <img id="logo" src="Images site web/12650659_886332674816078_425547975_n.mini.jpg" alt="Photo de sport" title="Play4all"/> 
-                     </a> 
-                  
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=le_site_du_sport;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
 
-
-                     <h1> -Le site du sport - </h1>
-                     
-                 <nav>
-                     <div id="Home">Home</div> 
-                     <a href="Directory.html"id="Directory">Directory</a>
-                     <a href="Group.html"id="Group">Group</a>  
-                     <a href="Forums.html"id="Forums">Forums</a> 
-                     <a href="Help.html" id="Help">Help</a> 
-                 </nav>
-             </header>
-     
-             <section>
+	 
+		
+			$req = $bdd->prepare('SELECT photo FROM utilisateur WHERE mail = ? ');
+			$req->execute(array(
+			$_SESSION['mail']));
+						//print_r ($req['id']);
+						//var_dump $req;
+						//die();
+			$resul= $req->fetch();
 			
-                 <article>
-                     <h2> Description </h2> 
-                     <div id= "presentation">Je jenmiche du 75 je veux jouer au foot avec toi
-                      </div>  </br>
-					  <div id="annonce">
-                     <div id= "news">News on Play4all</div></br></br>
-                     <div id="text"> Tom:"....." at xx/xx/xxxx</br></br>
-                     Anna:"....." at xx/xx/xxxx</br></br>
-                     ... </div>
-					 </div>
-                 </article>
-				 <fieldset>
-				 <div id="join">vos groupes </div></br> 
-				 <div id="text1">foot:"....."</br></br>
-                     winner:"....." at xx/xx/xxxx</br></br>
-                     ... </div>
-				 </fieldset>
+			echo $resul['photo'];
+    if (!empty($resul['photo'])){ ?>
+      <img src="IMG/avatar/<?php echo $resul['photo'];?>" alt="avatar" width="150" />
+    <?php
+    } ?>
+<p>nom:<?php if (isset($_SESSION['nom'])){echo htmlentities($_SESSION['nom']);} ?></p>
+<br/>
+<p>prenom:<?php if (isset($_SESSION['prenom'])){echo htmlentities($_SESSION['prenom']);} ?></p>
+<br/>
+<p>date de naissance:<?php if (isset($_SESSION['age'])){echo htmlentities($_SESSION['age']);} ?></p>
+<br/>
+<p>civilite:<?php if (isset($_SESSION['civilite'])){echo htmlentities($_SESSION['civilite']);} ?></p>
+<br/>
+<p>mail:<?php if (isset($_SESSION['mail'])){echo htmlentities($_SESSION['mail']);} ?></p>
+<br/>
+<p>description:<?php if (isset($_SESSION['description'])){echo htmlentities($_SESSION['description']);} ?></p>
+<br/>
 
-                 <aside>
-                 <a id="inscription" type="button" href="Inscription.html">deconnexion
-                 </a>
-                 
-				 <a id="joindre" type="button" href="">joindre un groupre
-                 </a>
+<a href = modification.php id ="modification">Modifier votre profil</a>
 
-                </div>
 
-                 <div id="oubli">
-                     <a href="Password.html">Forgot your Password?</a> </br>
-                     
-                     </div>
 
-                     <div id="calendrier">
-                     <iframe name="InlineFrame1" id="InlineFrame1" style="width:180px;height:220px;" src="http://www.mathieuweb.fr/calendrier/calendrier-des-semaines.php?nb_mois=1&nb_mois_ligne=4&mois=&an=&langue=fr&texte_color=B9CBDD&week_color=DAE9F8&week_end_color=C7DAED&police_color=453413&sel=true" scrolling="no" frameborder="0" allowtransparency="true"></iframe>
 
-                     </br> 
 
-                     <a href="https://www.facebook.com/"> <img id="facebook" src="Images site web/0facebook-miniature.jpg" alt="Photo de facebook" title="Facebook"/> </a>
-                     <a href="https://www.twitter.com/"> <img id="twitter"src="Images site web/Twitter_logo.mini.mini.png" alt="Photo de twitter" title="Twitter"/> </a>
-                     <a href="https://www.gmail.com/"> <img id="gmail" src="Images site web/images.mini.mini.mini.jpg" alt="Photo de gmail" title="Gmail"/> </a>
-                     </div>
 
-                 
-                   <div id="Sports">
-                     <h2> Our sports </h2>
-                     <p>
-                     <div id="Sport">
-                     <div id="Sport1">
-                     <a href="Baseball.html">Baseball</a> <a href="FootballUS.html">Football US </a> </br>
-                     </div>
-                     <div id="Sport2">
-                     <a href="Volleyball.html">Volleyball</a> 
-                     <a href="Basketball.html">Basketball</a> 
-                     </div>
-                     </div>
-                     </p>
-                   </div>
-                 </aside>
-             </section>
 
-             <footer>
-                     <a href="Francais.html"> <img id="france" src="Images site web/drapeauF.gif" alt="Drapeau de la France" title="Français"/> </a>
-                     <a href="Américain.html"> <img id="amerique" src="Images site web/flagus.mini.mini.mini.gif" alt="Drapeau des Etats Unis" title="Anglais"/> </a>
-             </footer>
-        </div>
-    </body>
- </html>
+</html>
